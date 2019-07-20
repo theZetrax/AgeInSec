ce504d62b6286eb1291a0a6865fcb82a82487b38
Zetrax <zabjd22@gmail.com>
2019-07-20 13:47:09 +0300

    Added UI for Users Added Date to Seconds Converter

--- AgeInSeconds/AgeInSeconds/Form1.Designer.cs
+++ AgeInSeconds/AgeInSeconds/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AgeInSeconds
 {
-    partial class Form1
+    partial class AgerForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Seconds = new System.Windows.Forms.TextBox();
+            this.Age = new System.Windows.Forms.DateTimePicker();
+            this.symbol = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Seconds
+            // 
+            this.Seconds.Location = new System.Drawing.Point(243, 12);
+            this.Seconds.Name = "Seconds";
+            this.Seconds.Size = new System.Drawing.Size(152, 20);
+            this.Seconds.TabIndex = 0;
+            // 
+            // Age
+            // 
+            this.Age.Location = new System.Drawing.Point(12, 12);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(200, 20);
+            this.Age.TabIndex = 1;
+            this.Age.ValueChanged += new System.EventHandler(this.Age_ValueChanged);
+            // 
+            // symbol
+            // 
+            this.symbol.AutoSize = true;
+            this.symbol.Location = new System.Drawing.Point(221, 15);
+            this.symbol.Name = "symbol";
+            this.symbol.Size = new System.Drawing.Size(16, 13);
+            this.symbol.TabIndex = 2;
+            this.symbol.Text = "△";
+            // 
+            // AgerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(408, 42);
+            this.Controls.Add(this.symbol);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.Seconds);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AgerForm";
+            this.ShowIcon = false;
+            this.Text = "Ager";
+            this.Load += new System.EventHandler(this.AgerForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.TextBox Seconds;
+        private System.Windows.Forms.DateTimePicker Age;
+        private System.Windows.Forms.Label symbol;
     }
 }
 
--- AgeInSeconds/AgeInSeconds/Form1.cs
+++ AgeInSeconds/AgeInSeconds/Form1.cs
@@ -10,11 +10,66 @@ using System.Windows.Forms;
 
 namespace AgeInSeconds
 {
-    public partial class Form1 : Form
+    
+    public partial class AgerForm : Form
     {
-        public Form1()
+        private void InitCustom()
+        {
+            this.Age.MaxDate = DateTime.Now;
+        }
+
+        public AgerForm()
         {
             InitializeComponent();
+            InitCustom();
+        }
+
+        private void AgerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Age_ValueChanged(object sender, EventArgs e)
+        {
+            int Years = DateTime.Now.Year - Age.Value.Year;
+            int Months = DateTime.Now.Month - Age.Value.Month;
+            int Days = DateTime.Now.Day - Age.Value.Day;
+
+            int output = ConvertDateToSeconds.ConvertYear(Years) + ConvertDateToSeconds.ConvertMonth(Months) + ConvertDateToSeconds.ConvertDay(Days);
+            Seconds.Text = output.ToString();
+        }
+    }
+
+    class ConvertDateToSeconds
+    {
+        public static int ConvertMinute(int Minute)
+        {
+            return Minute * 60;
+        }
+
+        public static int ConvertHour(int Hour)
+        {
+            return ConvertMinute(60) * Hour;
+        }
+
+        public static int ConvertDay(int Day)
+        {
+            return ConvertHour(24) * Day;
+        }
+
+        public static int ConvertWeek(int Week)
+        {
+            return ConvertDay(7) * Week;
+        }
+
+        public static int ConvertMonth(int Month)
+        {
+            return (ConvertWeek(4) + ConvertDay(2)) * Month;
+        }
+
+        public static int ConvertYear(int Year)
+        {
+            return ConvertMonth(12) * Year;
         }
     }
 }
